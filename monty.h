fd5764146cf4879c49b095dd8695a8fcd32aa338
komerela <kmwongera@icloud.com>
2019-05-16 19:16:48 +0000

    Format check fix

--- monty.h
+++ monty.h
@@ -19,9 +19,9 @@ char *global_line;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -34,8 +34,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        char *(*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	char *(*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* The following are prototypes */
